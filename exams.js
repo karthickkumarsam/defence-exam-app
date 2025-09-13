59c9ecc98a497f98bbdc51fc4dc2ba9eb476dbda
Karthick Kumar S E <karthikkumar@opengenx.com>
2025-09-13 17:03:17 +0530

    Added pdfs

--- exams.js
+++ exams.js
@@ -9,7 +9,8 @@ const exams = [
       { title: "Application Last Date", date: 'December 30, 2025 ' },
       { title: "Exam Date", date: 'April 12, 2026' }
     ],
-    url: "public/exams/CDS/NDA_Previous_Question.pdf"
+    url: "public/exams/NDA/NDA_Previous_Question.pdf",
+    syllabus: "public/exams/NDA/Syllabus.pdf"
   },
   {
     id: "cds",
@@ -23,7 +24,8 @@ const exams = [
       { title: "CDS 2 2026 Application Period", date: 'June 9, 2026 (last date)' },
       { title: "CDS 2 2026 Exam", date: 'September 13, 2026' }
     ],
-    url: "public/exams/CDS/CDS_Previous_Question.pdf"
+    url: "public/exams/CDS/CDS_Previous_Question.pdf",
+    syllabus: "public/exams/CDS/Syllabus.pdf"
   },
   {
     id: "afcat",
@@ -33,7 +35,8 @@ const exams = [
       { title: "For AFCAT 1 2026 (Courses starting July 2026)", date: 'Expected around December 2025.AFCAT 1 2026 Exam: Expected to beheld in February 2026.' },
       { title: "For AFCAT 2 2026 (Courses starting July 2026)", date: 'Expected in late May 2026.Detailed Notification: Expected around June 2026.' },
     ],
-    url: "public/exams/AFCAT/AFCAT_Previous_Question.pdf"
+    url: "public/exams/AFCAT/AFCAT_Previous_Question.pdf",
+    syllabus: "public/exams/AFCAT/Syllabus.pdf"
   }
 ];
 
